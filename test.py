e84bf33bd5d6694c8b927b236f2685f8ea45be47
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-06-20 15:20:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-'''
-PyPower Projects
-Emotion Detection Using AI
-'''
-
-#USAGE : python test.py
-
 from keras.models import load_model
 from time import sleep
 from keras.preprocessing.image import img_to_array
